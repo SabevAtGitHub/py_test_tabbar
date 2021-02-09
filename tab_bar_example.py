4b770ee177e458ba450ff01f7ab36964150c28fc
tt <tt3d@start.bg>
2021-02-09 15:41:49 +0100

    Initial commit

---
+++ tab_bar_example.py
@@ -0,0 +1,64 @@
+from PySide2.QtCore import *
+from PySide2.QtWidgets import *
+from PySide2.QtGui import *
+
+class TabBar(QTabBar):
+    def tabSizeHint(self, index):
+        s = QTabBar.tabSizeHint(self, index)
+        s.transpose()
+        return s
+
+    def paintEvent(self, event):
+        painter = QStylePainter(self)
+        opt = QStyleOptionTab()
+
+        for i in range(self.count()):
+            self.initStyleOption(opt, i)
+            painter.drawControl(QStyle.CE_TabBarTabShape, opt)
+            painter.save()
+
+            s = opt.rect.size()
+            s.transpose()
+            r = QRect(QPoint(), s)
+            r.moveCenter(opt.rect.center())
+            opt.rect = r
+
+            c = self.tabRect(i).center()
+            painter.translate(c)
+            painter.rotate(90)
+            painter.translate(-c)
+            painter.drawControl(QStyle.CE_TabBarTabLabel, opt);
+            painter.restore()
+
+
+class TabWidget(QTabWidget):
+    def __init__(self, *args, **kwargs):
+        QTabWidget.__init__(self, *args, **kwargs)
+        self.setTabBar(TabBar(self))
+        self.setTabPosition(QTabWidget.West)
+
+class ProxyStyle(QProxyStyle):
+    def drawControl(self, element, opt, painter, widget):
+        if element == QStyle.CE_TabBarTabLabel:
+            ic = self.pixelMetric(QStyle.PM_TabBarIconSize)
+            r = QRect(opt.rect)
+            w =  0 if opt.icon.isNull() else opt.rect.width() + self.pixelMetric(QStyle.PM_TabBarIconSize)
+            r.setHeight(opt.fontMetrics.width(opt.text) + w)
+            r.moveBottom(opt.rect.bottom())
+            opt.rect = r
+        QProxyStyle.drawControl(self, element, opt, painter, widget)
+
+if __name__ == '__main__':
+    import sys
+
+    app = QApplication(sys.argv)
+    QApplication.setStyle(ProxyStyle())
+    w = TabWidget()
+    w.addTab(QWidget(), QIcon("zoom.png"), "ABC")
+    w.addTab(QWidget(), QIcon("zoom-in.png"), "ABCDEFGH")
+    w.addTab(QWidget(), QIcon("zoom-out.png"), "XYZ")
+
+    w.resize(640, 480)
+    w.show()
+
+    sys.exit(app.exec_())
